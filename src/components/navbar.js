7132bcc2b3f4bc3d7086d2309c816fc1fe9b9041
Robert <robert.morgan0703@gmail.com>
2022-08-31 20:38:43 +0100

    notification badge

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,5 @@
+import Badge from 'react-bootstrap/Badge'
+
 import Notification from '../images/notif_bell.svg'
 import Plus from '../images/plus.svg'
 import Profile from '../images/profile.svg'
@@ -11,7 +13,7 @@ const Navbar = () => {
       <input type="text" name="emailAddress" placeholder="  Search by name, email, domain or phone number" value="" />
       <div id='nav-icons'>
         <button className='nav-icon'><img src={Plus} alt='Add New' /></button>
-        <button className='nav-icon'><img src={Notification} alt='Notification Bell' /></button>
+        <button className='nav-icon'><img src={Notification} alt='Notification Bell' /><Badge id='notif-num' bg="danger">47</Badge></button>
         <button className='nav-icon'><img src={Profile} alt='Profile' /></button>
         <button className='nav-icon'><img src={Question} alt='Question and FAQ' /></button>
       </div>
