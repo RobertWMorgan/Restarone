d38c6ff8ce080422c5651033d403a1d250e7663c
Robert <robert.morgan0703@gmail.com>
2022-09-01 08:31:48 +0100

    profile & styling

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,13 +1,26 @@
 import Badge from 'react-bootstrap/Badge'
 import LinkButtons from './link-buttons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+import Profile from '../../images/profile-image.png'
+
 const Sidebar = () => {
-  
-  return(
+
+  return (
     <aside id="sidebar">
       <div id='profile'>
-        
+        <div id='profile-image'>
+          <img src={Profile} alt='Profile' />
+        </div>
+        <div>
+          <p>Kelly Cheng</p>
+          <p id='subtext'>Copper</p>
+        </div>
+        <FontAwesomeIcon id='X' size='2x' inverse icon={faXmark} />
       </div>
-        <LinkButtons />
+      <LinkButtons />
     </aside>
   )
 }
