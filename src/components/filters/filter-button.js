15d29bbc22eeb1f6f0c91dc31f9bd7f66b0b9ba0
Robert <robert.morgan0703@gmail.com>
2022-09-01 07:16:12 +0100

    added map index

--- src/components/filters/filter-button.js
+++ src/components/filters/filter-button.js
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowDownShortWide, faArrowUpWideShort } from '@fortawesome/free-solid-svg-icons'
 
 const FilterButton = ({ filterList }) => {
-  const filterMap = filterList.map(filter => {
+  const filterMap = filterList.map((filter, index) => {
     return filter === 'First Name' ?
-      <button className='filter-button selected'>{filterList[0]}<FontAwesomeIcon icon={faArrowDownShortWide} /></button>
+      <button key={index} className='filter-button selected'>{filterList[0]}<FontAwesomeIcon icon={faArrowDownShortWide} /></button>
       :
       <button className='filter-button'>{filter}<FontAwesomeIcon icon={faArrowUpWideShort} /></button>
   })
