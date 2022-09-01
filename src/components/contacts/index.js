e18d930c28662dc9e4ab81d9737a9d46ea6bc8a9
Robert <robert.morgan0703@gmail.com>
2022-09-01 20:40:35 +0100

    fixed checkbox

--- src/components/contacts/index.js
+++ src/components/contacts/index.js
@@ -3,10 +3,17 @@ import ContactData from './contact-data'
 const Contactlist = () => {
   console.log(ContactData)
   const contactRow = ContactData.map((contact, index) => {
-    return <div key={index} className='contact-row'><span><button id='check-square' className='check-box'></button></span><span>{contact.name}</span> <span>{contact.company}</span> <span>{contact.title}</span> <span>{contact.email}</span> <span>{contact.owner}</span> <span>{contact.street}</span> <span>{contact.dateCreated}</span> <span>{contact.lastContact}</span></div>
+    return (
+      <div key={index} className='contact-row'>
+        <span><button id='check-square' className='check-box'></button></span>
+        <span>{contact.name}</span> <span>{contact.company}</span> <span>{contact.title}</span> <span>{contact.email}</span> <span>{contact.owner}</span> <span>{contact.street}</span> <span>{contact.dateCreated}</span> <span>{contact.lastContact}</span>
+      </div>
+    )
+
+
   })
 
-  return(
+  return (
     <main id='contacts'>
       {contactRow}
     </main>
